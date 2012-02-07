97de1387e8046ce765f468ba402b2c7d020b7851
Anders <MrYdremark@gmail.com>
2012-02-07 12:27:51 +0100

    fixed header_parser.

--- header_parser.c
+++ header_parser.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <regex.h>
 
 #include "header_parser.h"
 
@@ -13,17 +14,27 @@ void to_lower(char * in, char * out, int size) {
 
 struct ParsedHeader header_parser(char * input) {
   struct ParsedHeader h;
-  char end = '\r';
-  char * header = malloc(strlen(input));
-  char *result_begin, *begin = "host:";
-  size_t result_size = 0;
-  memcpy(header, input, strlen(input));
-  to_lower(input, header, strlen(input));
-  result_begin = (strstr(header, begin) + 6);
-  char * result_end = strchr(result_begin, end);
-  result_end[0] = '\0';
-  //char result[strlen(result_begin)] = result_begin;
-  free(header);
+  int status;
+  regex_t regex;
+  char * result_begin = NULL;
+
+  regcomp(&regex, "host:", REG_EXTENDED|REG_ICASE|REG_NOSUB);
+  if((status = regexec(&regex, input, (size_t) 0, NULL, 0)) == 0) {
+    char end = '\r';
+    char * header = malloc(strlen(input));
+    char *begin = "host:";
+    size_t result_size = 0;
+    memcpy(header, input, strlen(input));
+    to_lower(input, header, strlen(input));
+    result_begin = (strstr(header, begin) + 6);
+    fprintf(stderr, "%s", result_begin);
+    char * result_end = strchr(result_begin, end);
+    result_end[0] = '\0';
+    //char result[strlen(result_begin)] = result_begin;
+    free(header);
+  }
+  
+  regfree(&regex);
   
   h.header = input;
   h.host = result_begin;
